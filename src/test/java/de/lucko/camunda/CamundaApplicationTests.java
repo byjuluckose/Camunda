1725ca8e47875ba4851feebf1c69d1f780851a37
Luckose <byjuluckose@gmail.com>
2022-01-30 21:59:34 +0100

    Initial Commit

---
+++ src/test/java/de/lucko/camunda/CamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lucko.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
